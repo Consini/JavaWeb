62ee3d73bfb518198ec06c7f3c1ab8089bb9fe01
Consini <2459971164@qq.com>
2019-12-09 22:32:00 +0800

    练习2

---
+++ 12-01-博客系统的JDBC演示/src/Lianxi2.java
@@ -0,0 +1,14 @@
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2019/12/6 21:17
+ **/
+//import com.mysql.jdbc.Driver;验证jar、包是否加入成功
+//Connection 一条 mysql 链接
+// Statement 一个 mysql 语句
+public class Lianxi2 {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+
+    }
+}
