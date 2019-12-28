27549b27a435bb693835c609c358692ef1fc936e
Consini <2459971164@qq.com>
2019-12-28 16:44:30 +0800

    UDP客户端与服务器端实现简单英译汉

--- 12-07套接字/src/udp/echo/Client.java
+++ 12-07套接字/src/udp/echo/Client.java
@@ -2,6 +2,7 @@ package udp.echo;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.*;
+import java.util.Scanner;
 
 /**
  * @Description TODO
@@ -10,38 +11,43 @@ import java.net.*;
  **/
 public class Client {
     public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
         //新建 DatagramSocket
-        DatagramSocket udpClientSocket = new DatagramSocket();
-        // 往对方发数据
-        String message = "0_0";
-        byte[] sendBuffer = message.getBytes("UTF-8");
-        //得到服务器IP地址
-        //127.0.0.1 本机地址
-        byte[] serverIP = new byte[4];
-        serverIP[0] = (byte)127;
-        serverIP[1] = (byte)0;
-        serverIP[2] = (byte)0;
-        serverIP[3] = (byte)1;
-        InetAddress serverAddress = InetAddress.getByAddress(serverIP);
-        DatagramPacket sendPacket = new DatagramPacket(
-                sendBuffer,
-                sendBuffer.length,
-                serverAddress,
-                9898
-        );
-        udpClientSocket.send(sendPacket);
-        // 显示收到的消息
-        byte[] receiveBuffer = new byte[1024];
-        DatagramPacket receivePacket = new DatagramPacket(
-                receiveBuffer, receiveBuffer.length);
-        udpClientSocket.receive(receivePacket);
-        String responseMessage = new String(
-                receivePacket.getData(),
-                0,
-                receivePacket.getLength(),
-                "UTF-8"
-        );
-        System.out.println(responseMessage);
-        udpClientSocket.close();
+        DatagramSocket Socket = new DatagramSocket();
+        while(true) {
+            // 往对方发数据
+            //String message = "Hello! 0_0";//固定的
+            String message = sc.nextLine();
+            byte[] sendBuffer = message.getBytes("UTF-8");
+            //得到服务器IP地址
+            //127.0.0.1 本机地址
+            byte[] serverIP = new byte[4];
+            serverIP[0] = (byte) 127;
+            serverIP[1] = (byte) 0;
+            serverIP[2] = (byte) 0;
+            serverIP[3] = (byte) 1;
+            InetAddress serverAddress = InetAddress.getByAddress(serverIP);
+            DatagramPacket sendPacket = new DatagramPacket(// 构建发送报文
+                    sendBuffer,
+                    sendBuffer.length,
+                    serverAddress,
+                    9898
+            );
+            Socket.send(sendPacket);// 发送给对方
+
+            // 显示收到的消息
+            byte[] receiveBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(
+                    receiveBuffer, receiveBuffer.length);
+            Socket.receive(receivePacket);
+            String responseMessage = new String(
+                    receivePacket.getData(),
+                    0,
+                    receivePacket.getLength(),
+                    "UTF-8"
+            );
+            System.out.println(responseMessage);
+        }
+        //Socket.close();
     }
 }
--- 12-07套接字/src/udp/echo/Server.java
+++ 12-07套接字/src/udp/echo/Server.java
@@ -1,10 +1,10 @@
 package udp.echo;
-
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Description TODO
@@ -12,30 +12,47 @@ import java.net.SocketException;
  * @Date 2019/12/7 9:22
  **/
 public class Server {
+    //简单实现汉英字典
+    private static final Map<String,String> map = new HashMap<>();
+    static {
+        map.put("cat","猫咪");
+        map.put("dog","小狗");
+        map.put("rabbit","兔子");
+        map.put("lion","狮子");
+    }
     public static void main(String[] args) throws IOException {
-        // 1.新建 DatagramSocket
-        DatagramSocket udpServerSocket = new DatagramSocket(9898);
+        // 1.新建 DatagramSocket,并将其绑定到指定端口
+        DatagramSocket socket = new DatagramSocket(9898);
         while(true) {
-            byte[] receiveBuffer = new byte[1024];//存放收到的消息
-            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);// 数据报文
+            byte[] receiveBuf = new byte[1024];//存放收到的消息
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuf, receiveBuf.length);// 数据报文
             // 2.等客户端来撩
-            udpServerSocket.receive(receivePacket);// 接收到后通过 放到receiveBuffer
+            socket.receive(receivePacket);// 接收到后通过 放到receiveBuffer
             InetAddress clientAddress = receivePacket.getAddress();//对方的IP地址
             System.out.printf("从 %s：%d 收到了消息%n", clientAddress.getHostAddress(), receivePacket.getPort());
             System.out.printf("一共收到了 %d 字节的数据%n", receivePacket.getLength());
-            String message = new String(receivePacket.getData(), 0, receivePacket.getLength(), "UTF-8");
-            System.out.println(message);
+            String message = new String(receivePacket.getData(),
+                    0, receivePacket.getLength(),
+                    "UTF-8");
+
+            System.out.println(message); //打印接收到的消息
+            // 在 map 中查找收到的消息
+            String responseMassage = map.getOrDefault(message,"不会翻译");
+
             // 回给对方同样的消息
-            byte[] sendBuffer = message.getBytes("UTF-8");
+            //byte[] sendBuffer = message.getBytes("UTF-8");
+
+            // 回给对方对应单词的中文或者找不到该单词
+            byte[] sendBuffer = responseMassage.getBytes("UTF-8");
             DatagramPacket sendPacket = new DatagramPacket(
                     sendBuffer,
                     sendBuffer.length,
                     clientAddress,
                     receivePacket.getPort()
             );
-            udpServerSocket.send(sendPacket);
+            socket.send(sendPacket);
         }
         // 关闭 DatagramSocket
-        //udpServerSocket.close();
+        //socket.close();
     }
 }
