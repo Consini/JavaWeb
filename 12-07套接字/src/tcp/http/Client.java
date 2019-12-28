9125e1d41d4904488e88b31c5d0854988f5b32b1
Consini <2459971164@qq.com>
2019-12-28 16:43:55 +0800

    TCP客户端与服务器端实现聊天

--- 12-07套接字/src/tcp/http/Client.java
+++ 12-07套接字/src/tcp/http/Client.java
@@ -12,30 +12,33 @@ import java.util.Scanner;
 public class Client {
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
-        Socket tcpClientSocket = new Socket();
-        byte[] ipv4 = {(byte)192, (byte)168, (byte)0,(byte)196};
-        //byte[] ipv4 = {(byte)127, (byte)0, (byte)0,(byte)1};
-        InetAddress serverAddress = InetAddress.getByAddress(ipv4);
-        SocketAddress serverSocketAddress
-                = new InetSocketAddress(serverAddress, 8080);
-        tcpClientSocket.connect(serverSocketAddress);
+        Socket socket = new Socket();
+        //byte[] ipv4 = {(byte)192, (byte)168, (byte)0,(byte)196};
+        byte[] ipv4 = {(byte)127, (byte)0, (byte)0,(byte)1};// 服务器地址是本机
+        InetAddress serverAddress = InetAddress.getByAddress(ipv4);// 对方ip地址
+        SocketAddress serverSocketAddress = new InetSocketAddress(serverAddress, 8080);//对方端口号
+        // 连接
+        socket.connect(serverSocketAddress);
 
         while (true) {
-            System.out.print("请输出> ");
-            String request = scanner.nextLine();
+            System.out.print("请输入信息> ");
+            String request = scanner.nextLine();// 客户端的输入当作服务器端的输出
             // 通过字节流直接写入请求
-            OutputStream os = tcpClientSocket.getOutputStream();
+            OutputStream os = socket.getOutputStream();
             PrintStream out = new PrintStream(os, true, "UTF-8");
-            out.println(request);
+            out.println(request);// 打印出客户端的消息
 
-            // 通过字节流，直接读取数据
-            InputStream is = tcpClientSocket.getInputStream();
+            // 通过字节流，直接读取数据,服务器端的输出当作客户端的输入
+            InputStream is = socket.getInputStream();
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(is, "UTF-8")
             );
             String response = reader.readLine();
-            System.out.println(response);
+            System.out.print("对方回复您> ");
+            System.out.println(response);// 打印出服务器端回复的消息
         }
+        // 关闭
+        //socket.close();
     }
 }
 
--- 12-07套接字/src/tcp/http/Server.java
+++ 12-07套接字/src/tcp/http/Server.java
@@ -1,11 +1,7 @@
 package tcp.http;
-
 import java.io.*;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.Scanner;
-
 /**
  * @Description TODO
  * @Author K
@@ -13,15 +9,19 @@ import java.util.Scanner;
  **/
 public class Server {
     public static void main(String[] args) throws IOException, IOException {
-        ServerSocket tcpServerSocket = new ServerSocket(8080);
+        //监听 8080 端口
+        ServerSocket socket = new ServerSocket(8080);
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            Socket clientSocket = tcpServerSocket.accept();
-            InetAddress clientAddress = clientSocket.getInetAddress();
-            int clientPort = clientSocket.getPort();
-            //System.out.printf("有客户端连接上来 %s:%d%n",
-            //clientAddress.getHostAddress(), clientPort);
-            // 获取 字节流
+            // 等待连接上来
+            Socket clientSocket = socket.accept();// 接收消息
+
+            // 打印对方的ip和端口号
+//            InetAddress clientAddress = clientSocket.getInetAddress();
+//            int clientPort = clientSocket.getPort();
+//            System.out.printf("有客户端连接上来 %s:%d%n",clientAddress.getHostAddress(), clientPort);
+
+            // 获取字节流
             InputStream is = clientSocket.getInputStream();
             // 字节流转换为字符流
             InputStreamReader isReader = new InputStreamReader(is, "UTF-8");
@@ -34,10 +34,10 @@ public class Server {
 
             String line;
             while ((line = reader.readLine()) != null) {
-                System.out.println("好友说: " + line);
+                System.out.println("好友说>: " + line);// 输出客户端发送的消息
                 System.out.print("请回复> ");
-                String response = scanner.nextLine();
-                out.println(response);
+                String response = scanner.nextLine();// 输入待回复的消息
+                out.println(response);// 输出回复的消息
             }
         }
     }
