5bea9fe9328b10ba8b37eaaa316d3fac79b322c2
Consini <2459971164@qq.com>
2019-12-29 20:06:37 +0800

    建立TCP相关代码，等待客户端连接

---
+++ 12-29-HTTP/src/V1/SimpleHTTPServer.java
@@ -0,0 +1,15 @@
+package V1;
+import java.io.IOException;
+import java.net.*;
+import java.util.concurrent.*;
+
+public class SimpleHTTPServer {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1229);
+        ExecutorService pool = Executors.newFixedThreadPool(10);
+        while (true) {
+            Socket socket = serverSocket.accept();
+            pool.execute(new Task(socket));
+        }
+    }
+}
