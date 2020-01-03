324b7702a5dcf34135e24a7a6af0c8fe140c4d23
Consini <2459971164@qq.com>
2020-01-03 22:57:08 +0800

    Session服务器

--- 12-29-HTTP/src/V1/SessionServer.java
+++ 12-29-HTTP/src/V1/SessionServer.java
@@ -6,22 +6,21 @@ import business.User;
 import java.io.*;
 import java.util.UUID;
 
-public class SessionServer {
+public class SessionServer {//Session 服务器,用文件保存
     private static String dir = "会话";
 
+    //存用户信息，得到Session-id并返回
     public static String put(User user) throws IOException {
-        String sessionId = UUID.randomUUID().toString();
-        String filename = dir + "\\" + sessionId;
+        String sessionId = UUID.randomUUID().toString();//UUID:生成全球唯一随机码
+        String filename = dir + "\\" + sessionId;//文件名
         OutputStream os = new FileOutputStream(filename);
         ObjectOutputStream oos = new ObjectOutputStream(os);
-
-        oos.writeObject(user);
-
+        oos.writeObject(user);//写入信息
         os.close();
-
         return sessionId;
     }
 
+    // 传入Session-id，返回用户信息
     public static User get(String sessionId) throws IOException {
         String filename = dir + "\\" + sessionId;
 
@@ -30,7 +29,7 @@ public class SessionServer {
 
         User user = null;
         try {
-            user = (User)ois.readObject();
+            user = (User)ois.readObject();//从session-id文件中读出用户信息
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
