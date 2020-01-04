3fbe80dca20383a649a5da95efbda1e2eda059cf
Consini <2459971164@qq.com>
2020-01-04 21:46:25 +0800

    /login?username=用户名

--- 12-29-HTTP/src/V1/Servlet/LoginServlet.java
+++ 12-29-HTTP/src/V1/Servlet/LoginServlet.java
@@ -22,7 +22,7 @@ public class LoginServlet extends HttpServlet {
             return;
         }
 
-        User currentUser = User.findUser(username);
+        User currentUser = User.findUser(username);// 查找当前用户是否存在
         if (currentUser == null) {
             resp.setStatus("401 Unauthorized");
             resp.println("<h1>该用户不存在</h1>");
@@ -31,7 +31,9 @@ public class LoginServlet extends HttpServlet {
 
         String sessionId = SessionServer.put(currentUser);
 
-        resp.setHeader("Set-Cookie", "session-id=" + sessionId + "; expires=Tue, 07-Apr-2020 08:46:16 GMT; Max-Age=8640000");
+        //种Cookie，将session-id存入
+        resp.setHeader("Set-Cookie", "session-id=" + sessionId + "; " +
+                "expires=Tue, 07-Apr-2020 08:46:16 GMT; Max-Age=8640000");
 
 //        //resp.setHeader("Set-Cookie","username= "+username);
 //        // 设置 Cookie 时间
