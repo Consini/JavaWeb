ae1279f13b773066fad7c0d1ab02a29c18a03a6e
Consini <2459971164@qq.com>
2020-01-04 21:43:59 +0800

    建立TCP相关连接，等待客户端连接

--- 12-29-HTTP/src/V1/SimpleHTTPServer.java
+++ 12-29-HTTP/src/V1/SimpleHTTPServer.java
@@ -5,7 +5,7 @@ import java.util.concurrent.*;
 
 public class SimpleHTTPServer {
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1229);
+        ServerSocket serverSocket = new ServerSocket(80);
         ExecutorService pool = Executors.newFixedThreadPool(10);
         while (true) {
             Socket socket = serverSocket.accept();
