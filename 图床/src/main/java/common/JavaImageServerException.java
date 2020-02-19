34383fe7d1ac5d5d5490253d21ad2f6f44d862de
Consini <2459971164@qq.com>
2020-02-19 22:50:45 +0800

    JavaImageServerException

---
+++ 图床/src/main/java/common/JavaImageServerException.java
@@ -0,0 +1,14 @@
+package common;
+
+import java.util.concurrent.Executor;
+
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2020/2/18 18:10
+ **/
+public class JavaImageServerException extends Exception {
+    public JavaImageServerException(String message){
+        super(message);
+    }
+}
