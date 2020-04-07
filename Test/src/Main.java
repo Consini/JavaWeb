27a367283a9f6d12561222b5afae52c0de78a59c
Consini <2459971164@qq.com>
2020-04-07 22:05:52 +0800

    Main

---
+++ Test/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * @Description TODO
+ * @Author K
+ * @Date 2020/2/28 19:09
+ **/
+public class Main {
+    public static void main(String[] args) {
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println(maxValue+1);
+        int minValue = Integer.MIN_VALUE;
+        System.out.println(minValue-1);
+    }
+}
